ef98e30db348c546c719f80a4a68b01cf560d582
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2022-02-16 00:15:31 +0700

    Create DonorSeeder.php

---
+++ database/seeders/DonorSeeder.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Donor;
+use Illuminate\Database\Seeder;
+
+class DonorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Donor::create([
+            'fullName' => 'Syahrul',
+            'gender' => 'rhesus-plus',
+            'placeBirth' => 'Sukabumi',
+            'dateBirth' => now(),
+            'mobile' => '0821123123',
+            'email' => 'mail@mail.com',
+            'bloodType' => 'blood-group-a',
+            'rhesus' => 'rhesus-plus',
+            'height' => '160',
+            'weight' => '60',
+            'positiveDate' => now(),
+            'negativeDate' => now(),
+            'agreement' => true,
+            'positiveImage' => 'positive.jpg',
+            'negativeImage' => 'negative.jpg'
+        ]);
+        Donor::create([
+            'fullName' => 'Syahrul',
+            'gender' => 'rhesus-plus',
+            'placeBirth' => 'Sukabumi',
+            'dateBirth' => now(),
+            'mobile' => '0821123123',
+            'email' => 'mail@mail.com',
+            'bloodType' => 'blood-group-a',
+            'rhesus' => 'rhesus-plus',
+            'height' => '160',
+            'weight' => '60',
+            'positiveDate' => now(),
+            'negativeDate' => now(),
+            'agreement' => true,
+            'positiveImage' => 'positive.jpg',
+            'negativeImage' => 'negative.jpg'
+        ]);
+        Donor::create([
+            'fullName' => 'Syahrul',
+            'gender' => 'rhesus-plus',
+            'placeBirth' => 'Sukabumi',
+            'dateBirth' => now(),
+            'mobile' => '0821123123',
+            'email' => 'mail@mail.com',
+            'bloodType' => 'blood-group-a',
+            'rhesus' => 'rhesus-plus',
+            'height' => '160',
+            'weight' => '60',
+            'positiveDate' => now(),
+            'negativeDate' => now(),
+            'agreement' => true,
+            'positiveImage' => 'positive.jpg',
+            'negativeImage' => 'negative.jpg'
+        ]);
+        Donor::create([
+            'fullName' => 'Syahrul',
+            'gender' => 'rhesus-plus',
+            'placeBirth' => 'Sukabumi',
+            'dateBirth' => now(),
+            'mobile' => '0821123123',
+            'email' => 'mail@mail.com',
+            'bloodType' => 'blood-group-a',
+            'rhesus' => 'rhesus-plus',
+            'height' => '160',
+            'weight' => '60',
+            'positiveDate' => now(),
+            'negativeDate' => now(),
+            'agreement' => true,
+            'positiveImage' => 'positive.jpg',
+            'negativeImage' => 'negative.jpg'
+        ]);
+    }
+}
